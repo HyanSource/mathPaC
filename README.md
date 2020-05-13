3a208f9440ddbc63bf73a66f48a26e202129d28f
Hyan <s815651@yahoo.com.tw>
2020-05-13 21:30:41 +0800

    permutations and comtination

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # mathPaC
 排列組合公式包
+
+- F (factorial)
+階層公式
+- P (permutations)
+排列公式
+- C (combination)
+組合公式
+- H (combination with repetition)
+重複組合公式
---
+++ mathPaC_test.go
@@ -0,0 +1,25 @@
+package mathPaC
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestF(t *testing.T) {
+	fmt.Println(F(5))
+	fmt.Println(F(4))
+}
+
+func TestP(t *testing.T) {
+	fmt.Println(P(5, 3))
+	fmt.Println(P(10, 5))
+}
+
+func TestC(t *testing.T) {
+	fmt.Println(C(5, 2))
+	fmt.Println(C(2, 2))
+}
+
+func TestH(t *testing.T) {
+
+}
